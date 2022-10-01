04613e72adf02cc2b81b1826126df06fbfd036ec
Felipe Lauton <felipelauton021@gmail.com>
2022-10-01 17:01:20 -0300

    :sparkles: feat: Arrays e Slices

---
+++ 9-Arrays e Sclices/arrays-e-slices.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Arrays e Slices")
+
+	var array1 [5]string
+	array1[0] = "Posição 1"
+	fmt.Println(array1)
+
+	array2 := [5]string{"posição 1", "posição 2", "posição 3", "posição 4", "posição 5"}
+	fmt.Println(array2)
+
+	array3 := [...]int{1, 2, 3, 4}
+	fmt.Println(array3)
+
+	slice := []int{19, 230, 34}
+	fmt.Println(slice)
+
+	slice = append(slice, 18)
+	fmt.Println(slice)
+
+	slice2 := array2[1:3]
+	fmt.Println(slice2)
+
+	array2[1] = "Posição alterada"
+	fmt.Println(slice2)
+}
