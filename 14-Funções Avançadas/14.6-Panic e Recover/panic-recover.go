f9032f1b8fcbf1b525a3e0cc0177fdd5bcbc0366
Felipe Lauton <felipelauton021@gmail.com>
2022-10-03 15:27:56 -0300

    :sparkles: feat: Panic e Recover

---
+++ 14-Funções Avançadas/14.6-Panic e Recover/panic-recover.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(alunoEstaAprovado(6 , 7))
+	fmt.Println("Pós execução")
+
+}
+
+func alunoEstaAprovado(n1, n2 float64) bool {
+	defer recuperarExecucao()
+	media := (n1 + n2) / 2
+
+	if media > 6 {
+		return true
+	} else if media < 6 {
+		return false
+	}
+
+	panic("A MÉDIA É EXATAMENTE 6!")
+}
+
+func recuperarExecucao(){
+	if r := recover(); r != nil{
+		fmt.Println("Execução recuperada")
+	}
+}
