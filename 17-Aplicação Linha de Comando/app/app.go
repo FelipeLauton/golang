84579594449dd7fb587396fdbb71cda3c35ecc0e
Felipe Lauton <felipelauton021@gmail.com>
2022-10-04 15:16:05 -0300

    :sparkles: feat: Busca por servidores adicionada

--- 17-Aplicação Linha de Comando/app/app.go
+++ 17-Aplicação Linha de Comando/app/app.go
@@ -14,17 +14,25 @@ func Gerar() *cli.App{
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca IPs e Nomes de Servidor na Internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name: "host",
+			Value: "devbook.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
 			Name: "ip",
 			Usage: "Busca IPs de endereços na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name: "host",
-					Value: "devbook.com.br",
-				},
-			},
+			Flags: flags,
 			Action: BuscarIps,
+		},
+		{
+			Name: "servidores",
+			Usage: "Busca o nome dos servidores na internet",
+			Flags: flags,
+			Action: buscarServidores,
 		},
 	}
 	return app
@@ -40,5 +48,18 @@ func BuscarIps(c *cli.Context){
 
 	for _, ip := range ips{
 		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context){
+	host := c.String("host")
+
+	servidores, erro := net.LookupNS(host)
+	if erro != nil{
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores{
+		fmt.Println(servidor)
 	}
 }
