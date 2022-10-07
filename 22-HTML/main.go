e48353549a423d9bb16887a6be7e88d8aae9b353
Felipe Lauton <felipelauton021@gmail.com>
2022-10-07 09:41:44 -0300

    :sparkles: feat: Renderizando página HTML

---
+++ 22-HTML/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"html/template"
+)
+
+var templates *template.Template
+
+type usuario struct{
+	Nome string
+	Email string 
+}
+
+func main() {
+
+	templates = template.Must(template.ParseGlob("*.html"))
+
+	http.HandleFunc("/home", func (w http.ResponseWriter, r *http.Request) {
+
+		u := usuario{
+			"Julio",
+			"JulioGameplaysGemaPLS@gmail.com",
+		}
+
+		templates.ExecuteTemplate(w, "home.html", u)
+	})
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
