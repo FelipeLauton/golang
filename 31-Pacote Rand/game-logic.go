82a9260f49905fe896833e9aed92226a23d55b15
Felipe Lauton <felipelauton021@gmail.com>
2022-11-24 12:43:44 -0300

    :sparkles: feat: Jogo criado

--- 31-Pacote Rand/game-logic.go
+++ 31-Pacote Rand/game-logic.go
@@ -3,15 +3,44 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
 	"time"
+)
+
+const (
+	maxTurns = 5
+	usage = `Welcome to the Luck Number Game!
+	
+The program will pick %d random numbers.
+Your mission is to guess one those numbers.
+
+The greater your number is, the harder it gets.
+
+Wanna play?`
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	guess := 10
+	args := os.Args[1:]
+	if len(args) != 1 {
+		fmt.Printf(usage, maxTurns)
+		return
+	}
 
-	for turn := 0; turn < 5; turn++{
+	guess, err := strconv.Atoi(args[0]) 
+	if err != nil {
+		fmt.Println("Not a number.")
+		return
+	}
+
+	if guess < 0 {
+		fmt.Println("Please pick a positive number.")
+		return
+	}
+
+	for turn := 0; turn < maxTurns; turn++{
 		n := rand.Intn(guess + 1)
 
 		if n == guess {
