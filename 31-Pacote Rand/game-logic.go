b57172388ec4ab946230ba0d8e78aa59384b0123
Felipe Lauton <felipelauton021@gmail.com>
2022-11-24 12:13:49 -0300

    :sparkles: feat: Lógica do Game feita

---
+++ 31-Pacote Rand/game-logic.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	guess := 10
+
+	for turn := 0; turn < 5; turn++{
+		n := rand.Intn(guess + 1)
+
+		if n == guess {
+			fmt.Println("YOU WIN")
+			return
+		}
+	}
+	fmt.Println("You Lost... try again?")
+}
