6afd767adc58ee64160c9852817fed2d7cf85c96
Felipe Lauton <felipelauton021@gmail.com>
2022-11-25 17:12:49 -0300

    :sparkles: feat: Breaking from a Switch using Labels

--- 32-Word Finder/word-finder.go
+++ 32-Word Finder/word-finder.go
@@ -15,7 +15,13 @@ func main() {
 
 queries:
 	for _, q := range query {
+	search:
 		for i, w := range words {
+			switch q {
+			case "and", "or", "the":
+				break search
+			}
+
 			if q == w {
 				fmt.Printf("#%-2d: %q\n",
 					i+1, w)
