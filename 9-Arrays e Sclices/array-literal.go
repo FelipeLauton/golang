c1b80135a2513da14f7c21a5dd22ee469ffceb22
Felipe Lauton <felipelauton021@gmail.com>
2022-11-30 15:38:06 -0300

    :sparkles: feat: Criando Arrays Literals

---
+++ 9-Arrays e Sclices/array-literal.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	books := [4]string{
+		"Kafka's Revenge",
+		"Stay Golden",
+		"Everythingship",
+		"Kafka's Revenge 2nd Edition",
+	}
+
+	fmt.Println(books)
+}
