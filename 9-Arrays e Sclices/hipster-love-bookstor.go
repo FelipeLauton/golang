83b1e563e644a758442c0a0b1d304e8223e7baf3
Felipe Lauton <felipelauton021@gmail.com>
2022-11-30 14:18:01 -0300

    :sparkles: feat: Utilizando for range nos arrays

--- 9-Arrays e Sclices/hipster-love-bookstor.go
+++ 9-Arrays e Sclices/hipster-love-bookstor.go
@@ -19,4 +19,34 @@ func main() {
 	fmt.Println("books :", books)
 	fmt.Printf("books : %q\n", books)
 	fmt.Printf("books : %#v\n", books)
-}
+
+	var (
+		wBooks [winter]string
+		sBooks [summer]string
+	)
+
+	wBooks[0] = books[0]
+	//sBooks[0] = books[1]
+	//sBooks[1] = books[2]
+	//sBooks[2] = books[3]
+
+	for i := range sBooks {
+		sBooks[i] = books[i + 1]
+	}
+
+	fmt.Printf("\nwinter : %#v\n", wBooks)
+	fmt.Printf("\nsummer : %#v\n", sBooks)
+
+	var published [len(books)]bool
+
+	published[0] = true
+	published[len(books)-1] = true
+
+	fmt.Println("\nPublished Books:")
+	for i, ok := range published {
+		if ok {
+			fmt.Printf("+ %s\n", books[i])
+		}
+	}
+
+} 
