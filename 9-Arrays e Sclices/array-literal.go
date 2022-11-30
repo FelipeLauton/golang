c8f8e8c03efe79fa19f1929a5472249c27a26f6b
Felipe Lauton <felipelauton021@gmail.com>
2022-11-30 15:42:18 -0300

    Adicionando comentarios sobre arrays

--- 9-Arrays e Sclices/array-literal.go
+++ 9-Arrays e Sclices/array-literal.go
@@ -6,6 +6,10 @@ import (
 
 func main() {
 
+	// A utilização de arrays se faz necessaria para trazer mais performance em seus códigos.
+	// Em Go, utilizamos comumente slices por não se limitarem a tamanhos pré definidos.
+	// Abaixo temos a declaração de um Array literal com o declarador curto de declaração, o tamanho,
+	// e os dados do Arrary separados por virgula.	
 	books := [4]string{
 		"Kafka's Revenge",
 		"Stay Golden",
