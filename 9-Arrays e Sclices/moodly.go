dd2da2a26f2c4e26f3453034bb61149623893d39
Felipe Lauton <felipelauton021@gmail.com>
2022-12-01 09:53:13 -0300

    :sparkles: feat: Criando Programa Moodly

---
+++ 9-Arrays e Sclices/moodly.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+// Nesse código temos um pequeno desafio de receber o nome de um usuario e retornar como seu nome e como ele está se sentindo.
+// Primeiro inicio meu código trazendo o uma randomização do pacote rand que muda conforme o tempo avança.
+// Depois criei uma variavel do tipo os.Args para receber os argumentos do usuario passado [1:] para pegar somente o que for passado.
+// Após isso criei uma validação para verificar se o usuario digitou seu nome ou não, caso não o programa encerra.
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	args := os.Args[1:]
+
+	if len(args) != 1 {
+		fmt.Println("[your name]")
+		return
+	}
+	
+
+	moods := [5]string{
+		"sad",
+		"terrible",
+		"happy",
+		"awesome",
+		"good",
+	}
+
+	for turn := 0; turn < len(moods); turn++ {
+		n := rand.Intn(len(moods))
+
+		fmt.Printf("%v feels %v", args[1], moods[n])
+		return
+	}
+}
