f62d18583f72a1d2a4d41db6391d40893fe945a7
Felipe Lauton <felipelauton021@gmail.com>
2022-12-13 21:39:36 -0300

    feat: Comparing slices

--- 9-Arrays e Sclices/array-and-slices.go
+++ 9-Arrays e Sclices/array-and-slices.go
@@ -6,9 +6,39 @@ func main() {
 	var books [5]string
 
 	books[0] = "dracula"
-	books[1] = "star wars"
+	books[1] = "1984"
+	books[2] = "island"
 
-	games := []string{"sekiro", "sta wars"}
+	newBooks := [5]string{"ulysses", "fire"}
+	books = newBooks
 
-	fmt.Println(books, games)
+	games := []string{"sekiro", "fallen order"}
+	newGames := []string{"pacman", "doom"}
+
+	newGames = games
+	games = nil
+
+	games = []string{}
+
+	var ok string
+	for i, game := range games {
+		if game != newGames[i] {
+			ok = "not "
+			break
+		}
+	}
+
+	if len(games) != len(newGames) {
+		ok = "not "
+	}
+
+	fmt.Printf("games and newGames are %sequal\n\n", ok)
+
+	fmt.Printf("books			: %#v\n", books)
+	fmt.Printf("new books		: %#v\n", newBooks)
+	fmt.Printf("games			: %#v\n", games)
+
+	fmt.Printf("games			: %T\n", games)
+	fmt.Printf("games' len		: %d\n", len(games))
+	fmt.Printf("nil? 			: %t\n", games == nil)
 }
